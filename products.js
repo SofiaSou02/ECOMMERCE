812d5da01a75d570ef1e2859fae49f688c6d596b
Sofia Souto <54673134+SofiaSou02@users.noreply.github.com>
2019-09-07 15:05:36 -0300

    Add files via upload

---
+++ products.js
@@ -0,0 +1,52 @@
+var productsArray = [];
+var currentProductsArray = [];
+
+
+//function sortProducts(criteria, array){}
+
+
+function showProductsList(currentProductsArray){
+    let htmlContentToAppend = "";
+    for(let i = 0; i < currentProductsArray.length; i++){
+        let products = currentProductsArray[i];
+
+        //if (((minCount == undefined) || (minCount != undefined && parseInt(products.productCount) >= minCount)) &&
+        //    ((maxCount == undefined) || (maxCount != undefined && parseInt(products.productCount) <= maxCount))){
+
+            htmlContentToAppend += `
+            <a href="products-info.html" class="list-group-item list-group-item-action">
+                <div class="row">
+                    <div class="col-3">
+                        <img src="` + products.imgSrc + `" alt="` + products.description + `" class="img-thumbnail">
+                    </div>
+                    <div class="col">
+                        <div class="d-flex w-100 justify-content-between">
+                            <h4 class="mb-1">`+ products.name +`</h4>
+                            <small class="text-muted">` + products.cost + ` ` + products.currency + `</small>
+                        </div>
+                        <p class="mb-1">` + products.description + `</p>
+                    </div>
+                </div>
+            </a>
+            `
+        }
+
+        document.getElementById("cat-list-container").innerHTML = htmlContentToAppend;
+    }
+
+
+
+//Función que se ejecuta una vez que se haya lanzado el evento de
+//que el documento se encuentra cargado, es decir, se encuentran todos los
+//elementos HTML presentes.
+
+document.addEventListener("DOMContentLoaded", function(e){
+    getJSONData(PRODUCTS_URL).then(function(resultObj){
+        if (resultObj.status === "ok"){
+            currentProductsArray = resultObj.data;
+            //Muestro los productos 
+            showProductsList(currentProductsArray);
+        }
+    })
+});
+
